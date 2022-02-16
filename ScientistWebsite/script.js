42784d754866b0b790e3088417e738e004f8baca
Loacust <97564364+Loacust@users.noreply.github.com>
2022-02-15 23:31:55 -0700

    ScientistPersonalWebsite
    99% done

---
+++ ScientistWebsite/script.js
@@ -0,0 +1,13 @@
+
+ 
+function changecss() {
+    const el = document.getElementById("hope");
+    el.className = "hope2";
+    const el1 = document.getElementById("death");
+    el1.className = "hope3";
+    const el2 = document.getElementById("birth");
+    el2.className = "hope4";
+    }
+function display(){
+   document.write(document.getElementById('textBox'))
+}
