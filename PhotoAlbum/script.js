30eedc953391f435cef627f56bc944c7ca1ed107
Loacust <97564364+Loacust@users.noreply.github.com>
2022-02-25 14:18:31 -0700

    Final Set

--- PhotoAlbum/script.js
+++ PhotoAlbum/script.js
@@ -2,26 +2,74 @@ $(document).ready(function(){
     $(".pic").css("border", "black solid 5px");
     $(".tile_1_image").css({"font-family":"Pacifico, cursive", "font-size": "30px", "text-align": "center"});
     
-    $("#commentForm").validate({
-        rules: {
-            uname:  { 
-                required: true,
-                minlength: 8
-            },
-            imageTitle: "required",
-            comment: "required"
-            
-        },
-        messages: {
-            uname:{ 
-                required: "Please enter your Username",
-                minlength: "Your Username must be at least 8 characters"
-            },
-            imageTitle: "Please enter your image title",
-            comment: "Please write your comment"
-        }
+    
+    $('#usercheck').hide();
+    let usernameError = true;
+    $('#usernames').keyup(function(){
+        validateUsername();
     })
+    function validateUsername(){
+        let usernameValue = $('#usernames').val();
+        if (usernameValue.length <= 7) {
+            $('#usercheck').show();
+                usernameError = false;
+                return false;
+        }
+        else {
+            $('#usercheck').hide();
+            return true;
+        }
+    }
+    
+    
+    $('#imageTitleCheck').hide();
+    let imageNameError =true;
+    $('#imageTitle').keyup(function(){
+        validateImageName();
+    })
+    function validateImageName(){
+        let imageNameValue = $('#imageTitle').val();
+        if (imageNameValue.length == '') {
+            $('#imageTitleCheck').show();
+                imageNameError = false;
+                return false;
+        }
+        else {
+            $('#imageTitleCheck').hide();
+            return true;
+        }
+    }
+    
+    
+    $('#commentCheck').hide();
+    let commentError = true;
+    $('#comment').keyup(function(){
+        validateComment();
+    })
+    function validateComment(){
+        let commentValue = $('#comment').val();
+        if (commentValue.length == '') {
+            $('#commentCheck').show();
+                commentError = false;
+                return false;
+        }
+        else {
+            $('#commentCheck').hide();
+            return true;
+        }
+    }
+    $('#submitbtn').css('border','4px solid black')      
+    $('#submitbtn').click(function(){
+        validateUsername();
+        validateImageName();
+        validateComment();
+
+        if((usernameError == true) &&
+        (imageNameError == true) &&
+        (commentError == true)){
+        return true;}
+        else { return false;
+        }
 
     });
-
-    
+});
