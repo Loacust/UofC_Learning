8c6f96d3b778eb89e84fecf504206a55ad8b79ff
Loacust <97564364+Loacust@users.noreply.github.com>
2022-04-02 12:54:24 -0600

    update

---
+++ ROUTING_PROJECT/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Cmp1Component } from './components/cmp1/cmp1.component';
+import { Cmp3Component } from './components/cmp3/cmp3.component';
+import { Cmp2Component } from './components/cmp2/cmp2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    Cmp1Component,
+    Cmp3Component,
+    Cmp2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
