8be362a9b9791ef8ad466c92c10c2c1b498f2fe7
Loacust <97564364+Loacust@users.noreply.github.com>
2022-04-02 12:56:34 -0600

    Done

--- ROUTING_PROJECT/src/app/app-routing.module.ts
+++ ROUTING_PROJECT/src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Cmp1Component } from './components/cmp1/cmp1.component';
+import { Cmp2Component } from './components/cmp2/cmp2.component';
+import { Cmp3Component } from './components/cmp3/cmp3.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
+
 
 
 const routes: Routes = [
-  {path:'navbar',}
+  
+  {path:'cmp1', component:Cmp1Component},
+  {path:'cmp2', component:Cmp2Component},
+  {path:'cmp3', component:Cmp3Component},
+  {path:'**', component: PageNotFoundComponentComponent},
+  {path:'navbar', component: NavbarComponent}
 ];
 
 @NgModule({
--- ROUTING_PROJECT/src/app/app.module.ts
+++ ROUTING_PROJECT/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { Cmp1Component } from './components/cmp1/cmp1.component';
 import { Cmp3Component } from './components/cmp3/cmp3.component';
 import { Cmp2Component } from './components/cmp2/cmp2.component';
+import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Cmp2Component } from './components/cmp2/cmp2.component';
     NavbarComponent,
     Cmp1Component,
     Cmp3Component,
-    Cmp2Component
+    Cmp2Component,
+    PageNotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
